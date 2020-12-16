0fb384b6ba2f1ef395ed33b8ea60ec3d95449eb8
hiwot <hiwotgkassaye@gmail.com>
2020-12-16 11:39:13 -0500

    render a map with Google Maps on ViewComponent

--- src/components/ViewComponent.js
+++ src/components/ViewComponent.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import { baseUrl } from '../shared/baseUrl';
+
+import Map from './MapComponent';
 
 class View extends Component {
     constructor(props) {
@@ -18,10 +21,26 @@ class View extends Component {
     }
 
     render() {
+        const cords = this.state.listing.location
+        console.log(cords)
+        const uluru = { lat: -25.344, lng: 131.036 }
         return (
-            <div>
-                <h1>Hello World</h1>
-                <h1>{this.state.listing.name}</h1>
+            <div className='wrapper'>
+                {/* <Container> */}
+                    <Row>
+                        <Col md={6} >
+                            <Map center={cords} zoom={10}/>
+                        </Col>
+
+                        <Col>
+                            <h1>{this.state.listing.name}</h1>
+                            
+                        </Col>
+                    </Row>
+
+                {/* </Container> */}
+                
+                
             </div>
         )
     }
